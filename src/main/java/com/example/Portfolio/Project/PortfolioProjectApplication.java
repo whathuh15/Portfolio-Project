1f604fe3c379c7268e15ea72be2e1fa8855a9836
whathuh15 <whathuh15@gmail.com>
2018-04-06 16:57:06 -0500

    Portfolio project set-up

---
+++ src/main/java/com/example/Portfolio/Project/PortfolioProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.Portfolio.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioProjectApplication.class, args);
+	}
+}
